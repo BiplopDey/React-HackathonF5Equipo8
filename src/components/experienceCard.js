07e768b5c47e02ebe58200f54e2d831536383000
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-02-16 15:17:23 +0100

    Add styles and remove App.js

--- src/components/experienceCard.js
+++ src/components/experienceCard.js
@@ -1,7 +1,8 @@
+import "./experienceCard.css";
 export default function ExperienceCard({ id, guide, description }) {
   return (
     <div>
-      <h2>
+      <h2 className="hola">
         id: {id}, Guide Name: {guide}
       </h2>
       <h3>{description}</h3>
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "../App";
 import ExperienceView from "../pages/experienceView";
+import Home from "../pages/home";
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/experience" element={<ExperienceView />} />
       </Routes>
     </BrowserRouter>
