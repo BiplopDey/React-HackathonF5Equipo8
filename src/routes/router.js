8e671c06fed0ea0559e8a08032f4bfa3bdd9dd59
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-02-16 14:39:46 +0100

    Add styles

---
+++ src/routes/router.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "../App";
+import ExperienceView from "../pages/experienceView";
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/experience" element={<ExperienceView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
