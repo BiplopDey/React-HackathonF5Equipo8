822befcf2850b081e2f94fdc16f05cf1ddbfc3dd
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-02-17 13:34:10 +0100

    Button linked

--- src/components/experienceCard.js
+++ src/components/experienceCard.js
@@ -4,12 +4,13 @@ export default function ExperienceCard({ id, img, title }) {
   return (
     <div className="card card-block">
       <figure className="position-relative">
-          <img src={img} alt=""/>
-          <div className="card-featured position-absolute bottom-0">
-            <figcaption>{title}</figcaption>
-          
-            <button className="btn btn-primary">Ver más</button>
-          </div>
+        <img src={img} alt="" />
+        <div className="card-featured position-absolute bottom-0">
+          <figcaption>{title}</figcaption>
+          <Link to={`/experience/${id}`}>
+            <button className="btn btn-primary">Ver mas</button>
+          </Link>
+        </div>
       </figure>
     </div>
   );
