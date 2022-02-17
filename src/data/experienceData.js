ebd4dbdc4e2d135bc1ae05d341f7cdc44a4e0158
dordiales92 <dordiales92@gmail.com>
2022-02-17 22:26:57 +0100

    correction on data visualization

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -7,6 +7,14 @@ export const experienceList = [
 
   { id: 4, img: "https://img.freepik.com/foto-gratis/cerrar-pareja-bicicletas-electricas_23-2149098685.jpg?w=740", date: "mayo/2020", title: "Recorrido por Barcelona", guideName: "Maria Alvarez", description: "lorem", category: "Nature", price: "$200", location: "Barcelona" },
 
+  { id: 5, img: "https://img.freepik.com/free-photo/girl-raft_1398-1238.jpg?w=740", date: "junio/2020", title: "Rafting en Asturias", guideName: "Juan Perez", description: "lorem", category: "Nature", price: "$150", location: "Asturias" },
+  
+  { id: 6, img: "https://img.freepik.com/free-photo/beautiful-natural-landscape-forest_23-2149155618.jpg?w=740", date: "junio/2020", title: "Ruta natural en Bogota", guideName: "Marcos Carranzo", description: "lorem", category: "Nature", price: "$200", location: "Bogota" },
+
+  { id: 7, img: "https://img.freepik.com/free-photo/san-juan-de-gaztelugatxe-game-thrones-scenery-basque-country-spain_478841-643.jpg?w=740", date: "junio/2020", title: "Ruta por Gaztelugatxe", guideName: "Manuela Domingues", description: "lorem", category: "Urban", price: "$120", location: "Bilbao" },
+
+  { id: 8, img: "https://img.freepik.com/free-photo/sagrada-familia-cathedral-barcelona-spain-16-11-2019_225753-1339.jpg?w=740", date: "junio/2020", title: "Visita a la Sagrada Familia", guideName: "Maria Alvarez", description: "lorem", category: "Urban", price: "$100", location: "Barcelona" },
+
 
   // id, img, title, guideName, date, category
 ];
