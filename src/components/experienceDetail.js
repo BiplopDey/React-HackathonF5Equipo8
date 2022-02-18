5d813bfc18e91b51e857981f1726223a041c93d9
Marisa Posada <marisa.posada@gmail.com>
2022-02-18 09:50:51 +0100

    img de adv

--- src/components/experienceDetail.js
+++ src/components/experienceDetail.js
@@ -1,20 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../styles/components/ExperienceDetailStyle.css";
+import Header from "./Header";
+import NavBar from "./NavBar";
 
 function ExperienceDetail({ imageSource, title, description, price, name }) {
   return (
-    <div className="exp-detail card text-center">
-      <div className="overflow">
-        <img src={imageSource} alt="a wallpaper" className="" />
+      <div>
+          <Header/>
+          <div className="exp-detail card text-center">
+              <div className="overflow">
+                <img src={imageSource} alt="a wallpaper" className="" />
+              </div>
+              <div className="card-body text-light">
+                <h1 className="card-title text-center text-dark">{title}</h1>
+                <h2 className="card-title text-dark">{price} </h2>
+                <p className="card-text text-secondary">{name}</p>
+                <p className="card-text text-secondary">{description}</p>
+              </div>
+          </div>
+          <NavBar/>
       </div>
-      <div className="card-body text-light">
-        <h1 className="card-title text-center text-dark">{title}</h1>
-        <h2 className="card-title text-dark">{price} </h2>
-        <p className="card-text text-secondary">{name}</p>
-        <p className="card-text text-secondary">{description}</p>
-      </div>
-    </div>
   );
 }
 
